89347c7c8afa2c90a142e530d5d1468eb565b58b
Deron Kosloski <dsplace2010@hotmail.com>
2017-03-11 20:46:52 -0500

    Added counseling page  3-11-17dbk

---
+++ Career Rescue/Controllers/CounselingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Career_Rescue.Controllers
+{
+    public class CounselingController : Controller
+    {
+        // GET: Counseling
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
